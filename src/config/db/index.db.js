3d0e740a63d79aeb8abcfa9b30937f3532a00dee
TriTamLe <tritam.le54@gmail.com>
2023-01-23 22:26:34 +0700

    delete course

--- src/config/db/index.db.js
+++ src/config/db/index.db.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 
 async function connect() {
   try {
+    mongoose.set('strictQuery', true);
     await mongoose.connect('mongodb://127.0.0.1/heartmind_blog');
     console.log('Connected to database');
   } catch (error) {
-    console.log('Connecting failed!');
+    console.log('Connecting failed!', error);
   }
 }
 
